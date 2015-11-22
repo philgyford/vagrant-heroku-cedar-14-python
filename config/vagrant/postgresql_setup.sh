b8491f2efdaff980498563f9b24ac4cd3638ddca
Shepilov Vladislav <shepilov.v@progforce.com>
2015-11-22 06:25:31 +0300

    FIXED: `precise` -> `trusty` in postgresql_setup.sh
    Signed-off-by: Shepilov Vladislav <shepilov.v@progforce.com>

--- config/vagrant/postgresql_setup.sh
+++ config/vagrant/postgresql_setup.sh
@@ -59,7 +59,7 @@ PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
 if [ ! -f "$PG_REPO_APT_SOURCE" ]
 then
   # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > "$PG_REPO_APT_SOURCE"
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
 
   # Add PGDG repo key:
   wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
